8a4c34ea7dfcefc368a6fe5ead1a6006171995a5
Jeff Fohl <jeff@fohl.com>
2026-02-23 18:44:02 -0800

    more boilerplate

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+/**
+ *  Art is a constant process of inventing ways to escape the restrictions of past inventions.
+ */
+
+// types
+
+// constants
+const SWEETNESS = 24;
+const INTERVAL = 100;
+const HUE_RANGE = 40;
+const SATURATION_RANGE = 50;
+const LIGHTNESS_RANGE = 50;
+
+class WaterColorAnimator {
+  constructor(canvasId: string) {}
+
+  /**
+   * Initializes the animation on the specified canvas
+   */
+
+  public start() {
+    console.log("Starting the painting...");
+  }
+}
+
+const animator = new WaterColorAnimator("canvas");
+animator.start();
